cc7c69f63b06dd271a7552ed40fbfe17c0def88b
Sander De Puydt <sander.depuydt@pxl.be>
2024-09-23 18:28:10 +0200

    project created

---
+++ Ethias-Oefening/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ethias_Oefening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ETHIAS - GROET
+             * 
+             * Voorzie een programma dat door de verzekeringsmaatschappij
+             * Ethias gebruikt kan worden om klanten te registreren.
+             * 
+             * Maak één variabele aan, genaamd groet
+             *  - Zorg er voor dat je groet meerdere regels lang is
+             *  - Druk de groet af, zodra het programma start
+             * 
+             */
+            string groet = "?";
+
+
+
+            /*
+             * ETHIAS - REGISTRATIE FORMULIER
+             * Nadat de welkomsgroet getoond is, vraagt het programma om gegevens van de klant in te geven voor registratie
+             * 
+             * Maak een variabele voor elk van de volgende klantengegevens:
+             *  - Voornaam
+             *  - Achternaam
+             *  - Leeftijd
+             *  - IsMan (waar of niet waar)
+             *  - Telefoonnummer
+             */
+
+
+
+            /*
+             * ETHIAS - VERSLAG PRINTEN
+             * Nadat het programma alle gegevens heeft ontvangen van de klant
+             * wordt er een samenvatting afgedrukt in de console.
+             * 
+             * Gebruik string interpolatie om de variabelen te injecteren in een 
+             * samenvatting. Zorg er voor dat je samenvatting uitlijning bevat.
+             */
+
+
+
+            /*
+             * ETHIAS - TryParse
+             * Pas je Ethias registratie programma aan, zodat het niet meer kan
+             * crashen wanneer er ongeldige input wordt ingegeven.
+             * 
+             * Gebruik TryParse om je waardes te converteren.
+             */
+
+
+        }
+    }
+}
